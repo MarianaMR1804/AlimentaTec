2364b412c2023f2963bc414d8446f9732ccd52dc
celio bonilla <108300688+celiobovatola@users.noreply.github.com>
2024-12-03 12:52:16 -0600

    Mutacion updateJournal

--- src/main/java/com/example/alimentaTec/controller/JournalController.java
+++ src/main/java/com/example/alimentaTec/controller/JournalController.java
@@ -1,8 +1,5 @@
 package com.example.alimentaTec.controller;
 
-import org.modelmapper.ModelMapper;
-import org.modelmapper.PropertyMap;
-
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
@@ -19,9 +16,6 @@ public class JournalController {
 	@Autowired
 	private JournalService service;
 
-	@Autowired
-	private ModelMapper modelMapper;
-
 	@QueryMapping
 	public List<Journal> journals() {
 		return service.getAll();
@@ -31,11 +25,6 @@ public class JournalController {
 	public Journal journalById(@Argument Integer idJournal) {
 		return service.getIdJournal(idJournal);
 	}
-	/* 
-	@MutationMapping
-	public Journal addJournal(@Argument(value = "journal") JournalDTO journal) {
-		return service.save(convertToEntity(journal));
-	}*/
 
 	@MutationMapping
 	public Journal createJournal(@Argument int idSaucer, @Argument int idActivity, @Argument int idGoal,
@@ -48,13 +37,22 @@ public class JournalController {
 		return service.save(journalDTO);
 	}
 
-	// @MutationMapping
-	// public Journal updateJournal(@Argument Integer idJournal, @Argument(value =
-	// "journal") JournalDTO journalDTO) {
-	// Journal existingJournal = service.getIdJournal(idJournal);
-	// journalDTO.setIdJournal(existingJournal.());
-	// return service.save(journal);
-	// }
+	@MutationMapping
+	public Journal updateJournal(@Argument Integer idJournal, @Argument int idSaucer, @Argument int idActivity, 
+                             @Argument int idGoal, @Argument int idUser) {
+		Journal existingJournal = service.getIdJournal(idJournal);
 
-	
+		JournalDTO journalDTO = new JournalDTO();
+		journalDTO.setIdSaucer(idSaucer);
+		journalDTO.setIdActivity(idActivity);
+		journalDTO.setIdGoal(idGoal);
+		journalDTO.setIdUser(idUser);
+
+		existingJournal.setSaucer(service.getByIdSaucer(journalDTO.getIdSaucer()));
+		existingJournal.setPhysicalActivity(service.getByIdActivity(journalDTO.getIdActivity()));
+		existingJournal.setGoal(service.getByIdGoal(journalDTO.getIdGoal()));
+		existingJournal.setLogin(service.getByIdLogin(journalDTO.getIdUser()));
+
+		return service.save(existingJournal);
+	}
 }
